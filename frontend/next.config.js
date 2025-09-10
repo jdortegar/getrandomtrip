888cc7c549f917656d714f6634c94723c1143cf5
Santiago Senega <santiagosng@gmail.com>
2025-09-09 21:19:34 -0700

    feat: cambios aprobados en main

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -5,6 +5,7 @@ const nextConfig = {
     remotePatterns: [
       { protocol: 'https', hostname: 'images.unsplash.com' },
       { protocol: 'https', hostname: 'source.unsplash.com' },
+      { protocol: 'https', hostname: 'plus.unsplash.com' },
       { protocol: 'https', hostname: 'images.pexels.com' },
       { protocol: 'https', hostname: 'upload.wikimedia.org' },
       { protocol: 'https', hostname: 'i.pravatar.cc' },
